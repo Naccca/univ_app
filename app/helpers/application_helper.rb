c14ce818b7c14df5b868d2d9b05869b6c166adee
Nadezda Jeremic <nadezdajeremic@hotmail.com>
2019-05-05 16:55:51 +0200

    Seed and Sign up form

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,9 @@
 module ApplicationHelper
   def session_link
     if logged_in?
-      link_to('logout', logout_path, method: :delete)
+      link_to('Log out', logout_path, method: :delete)
     else
-      link_to('login', login_path)
+      link_to('Log in', login_path)
     end
   end
 end
